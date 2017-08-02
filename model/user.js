92104d11c83418f15204a5995758aa5f328c4d42
zhangzju <zhangway19921221@163.com>
2017-08-02 14:52:48 +0800

    add test user

--- model/user.js
+++ model/user.js
@@ -2,21 +2,20 @@ const userlist = [{
       id: '1',
       username: 'admin',
       password: 'admin',
-      email: 'zhangwei_w8284@tp-link.con.cn'
+      email: 'zhangwei_w8284@tp-link.com.cn'
     },{
       id: '2',
       username: 'zhangwei',
       password: '19921221',
-      email: 'zhangwei_w8284@tp-link.con.cn'
+      email: 'zhangwei_w8284@tp-link.com.cn'
+    },{
+      id: '3',
+      username: 'wangjiawei',
+      password: 'wangjiawei',
+      email: 'wangjiawei@tp-link.com.cn'
     }];
 
 module.exports.findOne = function(username, callback) {
-    // var user = {
-    //     id: '0',
-    //     username: 'anonymous',
-    //     password: '',
-    //     email: ''
-    // }
     
     for(i=0; i<=userlist.length;i++){
         if(userlist[i].username == username){
